890e1d34eeaf4485bfaeea05a02fdcf1ac75b860
lewnardu <lewnardu@yahoo.com.br>
2022-08-24 11:46:33 -0300

    Configurações heroku

--- duasperolasplussize/settings.py
+++ duasperolasplussize/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-#import django_heroku
+import django_heroku
 import os, sys
 from pathlib import Path
 from decouple import config
@@ -141,5 +141,4 @@ PROJECT_ROOT = os.path.dirname(__file__)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
