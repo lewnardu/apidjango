00adedc4cb0af83fb803d77f106d979947f5749c
lewnardu <lewnardu@yahoo.com.br>
2022-08-24 10:47:19 -0300

    Configurações heroku

--- duasperolasplussize/settings.py
+++ duasperolasplussize/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'duasperolasplussize.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.sqlite3",
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd8as61jbc7jnq1',
+        'USER': 'bjqapdfqixobfj',
+        'PASSWORD': 'e3b0d2955f05955f06a0093aef5e59399f4e6479f73b8f6ca1f02b3d0c55a7c7',
+        'HOST': 'ec2-44-205-112-253.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
